53727462e02979122f683bf8c1eeec5c71740dae
yoon <62196250+sda0503@users.noreply.github.com>
2024-01-11 08:53:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.18 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public string solution(int n) {
+        string answer = "";
+        for(int i=0; i< n; i++){
+            if(i % 2 == 1){
+                answer += "박" ;   
+            }else{
+                answer += "수";    
+            }
+            
+        }
+        return answer;
+    }
+}
