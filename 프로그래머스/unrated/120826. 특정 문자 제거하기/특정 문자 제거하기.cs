0b60c099d6e188621777742cdf40e6d08cb7ea76
yoon <62196250+sda0503@users.noreply.github.com>
2024-01-10 20:20:59 +0900

    [level unrated] Title: 특정 문자 제거하기, Time: 0.16 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, string letter) {
+        string answer = "";
+        string newString = my_string.Replace(letter,"");
+        return answer = newString;
+    }
+}
