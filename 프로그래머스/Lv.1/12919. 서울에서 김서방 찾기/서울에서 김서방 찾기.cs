905a092c7c7ba9016286e88629f4d84de4bc314a
yoon <62196250+sda0503@users.noreply.github.com>
2024-01-12 09:20:28 +0900

    [level Lv.1] Title: 서울에서 김서방 찾기, Time: 0.46 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public string solution(string[] seoul) {
+        string answer = "";
+        int x = Array.IndexOf(seoul,"Kim");
+        return answer = "김서방은 "+x.ToString()+"에 있다";
+    }
+}
