6f80ec0be278f3d3f25dc80ad46db79b44ceef6d
yoon <62196250+sda0503@users.noreply.github.com>
2024-01-14 17:50:03 +0900

    [level Lv.1] Title: 콜라츠 추측, Time: 0.17 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n = num;
+        for(int i = 0; i< 500; i++)
+        {
+            if(n == 1)
+            {
+                return i;
+            }
+            else
+            {
+                n = n % 2 == 0 ? n/2 : n * 3 + 1;    
+            }
+        }
+        answer = -1;
+        return answer;
+    }
+}
