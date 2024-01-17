87b659de02b595c64bb5932b8229f339e682bd29
yoon <62196250+sda0503@users.noreply.github.com>
2024-01-17 18:33:26 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.34 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cs
@@ -0,0 +1,15 @@
+using System;
+    
+public class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        double temp = Math.Sqrt(n);
+        if(temp % 1 != 0){
+            answer = -1;
+        }
+        else{
+            answer = ((long)temp + 1) * ((long)temp + 1);
+        }
+        return answer;
+    }
+}
