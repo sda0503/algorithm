0660fd8d9a614b092e469e61b9cccf5993069458
yoon <62196250+sda0503@users.noreply.github.com>
2024-01-18 09:12:17 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.45 ms, Memory: 31.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        List<int> answer = new List<int>(arr);
+        int min = arr.Min();
+        answer.RemoveAll(num => num == min);
+        if(answer.Count == 0)
+        {
+            answer.Add(-1);
+        }
+        return answer.ToArray();
+    }
+}
