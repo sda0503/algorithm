5880c9eb2f58f1af5084e2fe729e7a78f8bdf64f
yoon <62196250+sda0503@users.noreply.github.com>
2024-01-18 09:38:56 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.60 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public bool solution(string s) {
+        bool answer = false;
+        if(s.Length == 4 || s.Length ==6)
+        {
+            answer = int.TryParse(s, out int x);
+        }
+        return answer;
+    }
+}
