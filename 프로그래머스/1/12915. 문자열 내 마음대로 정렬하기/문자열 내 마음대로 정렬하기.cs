ffce257dc3133e0e345b44360b1359518b186533
yoon <62196250+sda0503@users.noreply.github.com>
2024-01-26 10:08:18 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 7.25 ms, Memory: 31.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cs
@@ -0,0 +1,11 @@
+using System.Linq;
+
+public class Solution {
+    public string[] solution(string[] strings, int n) {
+        string[] answer = new string[] {};
+        
+        answer = strings.OrderBy(x => x).OrderBy(x => x[n]).ToArray();        
+        
+        return answer;
+    }
+}
