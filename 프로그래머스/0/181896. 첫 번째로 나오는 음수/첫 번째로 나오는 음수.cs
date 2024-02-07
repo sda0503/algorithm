01680ff8af4be2423a070b64a8ecfad6df77d135
yoon <62196250+sda0503@users.noreply.github.com>
2024-02-07 20:31:17 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.16 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int answer = -1;
+        for(int i=0; i<num_list.Length; i++)
+        {
+            if(num_list[i] < 0)
+            {
+                return answer = i;
+            }
+        }
+        return answer;
+    }
+}
