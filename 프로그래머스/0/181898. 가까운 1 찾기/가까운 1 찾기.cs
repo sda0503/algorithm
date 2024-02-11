56b64320c8a236f084a452b936f90be7faf44360
yoon <62196250+sda0503@users.noreply.github.com>
2024-02-11 13:43:42 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = 0;
+        for(int i =idx; i<arr.Length; i++)
+        {
+            if(arr[i] == 1)
+            {
+                return answer = i;
+            }
+            else
+            {
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+}
