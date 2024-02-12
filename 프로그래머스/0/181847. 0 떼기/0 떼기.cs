faad582df5d897970b7dc32fc71b78630ed80259
yoon <62196250+sda0503@users.noreply.github.com>
2024-02-12 10:54:18 +0900

    [level 0] Title: 0 떼기, Time: 0.81 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public string solution(string n_str) {
+        string answer = "";
+        int changeInt = Convert.ToInt32(n_str);
+        return answer = changeInt.ToString();
+    }
+}
