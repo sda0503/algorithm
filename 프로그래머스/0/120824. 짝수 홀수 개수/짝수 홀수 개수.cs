8946109f23c652547339443a69c83eda1a791c53
yoon <62196250+sda0503@users.noreply.github.com>
2024-03-12 10:12:37 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        int num1 = 0;
+        int num2 = 0;
+        for(int i=0; i<num_list.Length; i++)
+        {
+            if(num_list[i] % 2 == 0) num1++;
+            else num2++;
+        }
+        answer[0] = num1;
+        answer[1] = num2;
+        return answer;
+    }
+}
