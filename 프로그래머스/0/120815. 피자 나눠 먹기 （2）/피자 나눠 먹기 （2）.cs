02ce775aa7decf8be21f9962d4e5fcbda9ef6120
yoon <62196250+sda0503@users.noreply.github.com>
2024-03-23 21:26:05 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.16 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; ;i++)
+        {
+            if((6 * i) % n == 0)
+            {
+                answer = i;
+                break;
+            }    
+        }
+        return answer;
+    }
+}
