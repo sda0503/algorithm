57dbc3d6f0ef8caf663560779bcdeb7b30ade43a
yoon <62196250+sda0503@users.noreply.github.com>
2024-06-03 15:48:44 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.54 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Solution {
+    public int solution(int n, string control) {
+        int answer = n;
+        char[] charArray = control.ToCharArray();
+        for (int i=0; i<control.Length; i++){
+             switch (control[i])
+            {
+                case 'w':
+                    answer += 1;
+                    break;
+                case 's':
+                    answer -= 1;
+                    break;
+                case 'd':
+                    answer += 10;
+                    break;
+                case 'a':
+                    answer -= 10;
+                    break;
+            }
+        }
+        
+        return answer;
+    }
+}
