cfa841a2a26cc68064871bd892fc1a5718fa9013
yoon <62196250+sda0503@users.noreply.github.com>
2024-06-11 14:30:53 +0900

    [level 0] Title: 배열 만들기 2, Time: 9.85 ms, Memory: 34.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int l, int r) {
+        int[] answer = new int[] {};
+
+        answer = Enumerable.Range(l, r - l + 1).Where(x => x.ToString().All(y=> y == '5' || y == '0')).ToArray();
+
+        if(answer.Length <= 0)
+            return new int[] {-1};
+
+        return answer;
+    }
+}
